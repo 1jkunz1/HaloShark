fd6cf95bdcc65d7b5e2c0481c7a1f4afbba7ac2a
Joe Kunzler <jkunzler@alphacoincapital.com>
2018-04-25 16:17:32 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,23 @@
 
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'JKMON'
+def record():
+    return render_template('record.html')
 
+@app.route('/picks')
+def picks():
+    return render_template('picks.html')
+
+
+@app.route('/picks')
+def picks():
+    return render_template('scraper.html')
+
+
+@app.route('/picks')
+def picks():
+    return render_template('forum.html')
