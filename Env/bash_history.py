390a32e7c75cf47ad687f8e209f0cea26e4d5a98
Joe Kunzler <jkunzler@alphacoincapital.com>
2018-04-25 16:40:20 -0500

    Add files via upload

---
+++ Env/bash_history.py
@@ -0,0 +1,17 @@
+unzip Wageroo
+pip install pillow
+pip install django debug toolbar
+pip install pyTZ
+unzip Misago-0.5.7
+unzip Misago-0.5.7 (2)
+Misago-0.5.7 (2).zip
+unzip Misago.zip
+cd Misago
+cd Misago-0.5.7
+cd Misago && vagrant up
+vagrant up
+cd Misago2
+cd Misago2/
+cd jkunzler
+cd home
+ls
